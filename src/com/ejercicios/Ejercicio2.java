6e73cbce3215df3e6ab0fdc29ddfd3b0464916fc
Aitor C. Figueroa <aitorcf@gmail.com>
2022-10-15 20:27:19 +0200

    Ejercicio 2 done

--- src/com/ejercicios/Ejercicio2.java
+++ src/com/ejercicios/Ejercicio2.java
@@ -1,5 +1,18 @@
 package com.ejercicios;
 
-public class Ejercicio2 {
+import java.util.Scanner;
 
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = "";
+        System.out.println("Introduce una palabra:");
+        str = input.nextLine();
+
+        if (str.contains("b") | str.contains("B")) {
+            System.out.println("La palabra " + str + " contiene la letra b");
+        } else {
+            System.out.println("La palabra " + str + " NO contiene la letra b");
+        }
+    }
 }
